6e81765087d36eb7db8b2c6a395d67ff583cde0b
Austin Hanson <austin.hanson@zuerchertech.com>
2019-08-29 15:03:15 -0500

    Removing preview LangVersion requirement

--- DotNet.Streams.Test/BasicMergeTests.cs
+++ DotNet.Streams.Test/BasicMergeTests.cs
@@ -12,28 +12,31 @@ namespace DotNet.Streams.Test
         [ClassData(typeof(SingleToManyTestStreams))]
         public async Task CanReadToEndOneMergesStream(MergedStream mergedStream, string expectedData)
         {
-            using var streamReader = new StreamReader(mergedStream);
-            var data = await streamReader.ReadToEndAsync();
-            Assert.Equal(expectedData, data);
+            using (var streamReader = new StreamReader(mergedStream))
+            {
+                var data = await streamReader.ReadToEndAsync();
+                Assert.Equal(expectedData, data);
+            }
         }
 
         [Theory]
         [ClassData(typeof(SingleToManyTestStreams))]
         public async Task CanReadMergedStreamsIncrementally(MergedStream mergedStream, string expectedData)
         {
-            using var streamReader = new StreamReader(mergedStream);
-
-            var totalData = new StringBuilder();
-            var totalBytesRead = 0;
-
-            while (!streamReader.EndOfStream)
+            using (var streamReader = new StreamReader(mergedStream))
             {
-                var buffer = new char[10];
-                var bytesRead = await streamReader.ReadAsync(buffer, 0, buffer.Length);
-                totalBytesRead += bytesRead;
-                totalData.Append(buffer, 0, bytesRead);
+                var totalData = new StringBuilder();
+                var totalBytesRead = 0;
 
-                Assert.Equal(expectedData.Substring(0, totalBytesRead), totalData.ToString());
+                while (!streamReader.EndOfStream)
+                {
+                    var buffer = new char[10];
+                    var bytesRead = await streamReader.ReadAsync(buffer, 0, buffer.Length);
+                    totalBytesRead += bytesRead;
+                    totalData.Append(buffer, 0, bytesRead);
+
+                    Assert.Equal(expectedData.Substring(0, totalBytesRead), totalData.ToString());
+                }
             }
         }
     }
